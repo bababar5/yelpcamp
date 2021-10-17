2d3ff0e391c5a597a4f165f8700b567e40a5a6c2
unknown <bababar5@gmail.com>
2021-10-17 12:29:13 +0300

    add start script

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,8 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   // useCreateIndex: true,
   useUnifiedTopology: true
